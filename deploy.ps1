e34630564405b91df31ee6ccf3abeb0baf025139
SYLVAIN <sylvain-externe.greneron@edf.fr>
2026-02-24 10:56:37 +0100

    test

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,9 @@
 # --- CONFIGURATION ---
-$API_GATEWAY_URL = "https://5mfczcfffk.execute-api.eu-west-3.amazonaws.com/prod"
+$API_GATEWAY_URL = "https://5mfczcfffk.execute-api.eu-west-3.amazonaws.com"
 $FRONTEND_PATH = "./frontend"
 $BACKEND_PATH = "./backend"
 
-Write-Host "?? Démarrage du déploiement StockHome..." -ForegroundColor Cyan
+Write-Host "?? Dï¿½marrage du dï¿½ploiement StockHome..." -ForegroundColor Cyan
 
 # 1. Build du Frontend
 Write-Host "?? Build du Frontend React..." -ForegroundColor Yellow
@@ -11,11 +11,11 @@ cd $FRONTEND_PATH
 $env:VITE_API_URL=$API_GATEWAY_URL
 npm run build
 
-# 2. Déploiement Backend + Frontend (S3/CloudFront)
-Write-Host "?? Déploiement sur AWS via Serverless..." -ForegroundColor Yellow
+# 2. Dï¿½ploiement Backend + Frontend (S3/CloudFront)
+Write-Host "?? Dï¿½ploiement sur AWS via Serverless..." -ForegroundColor Yellow
 cd ..
 cd $BACKEND_PATH
-# Suppression du cache pour éviter l'erreur de stream/zip
+# Suppression du cache pour ï¿½viter l'erreur de stream/zip
 if (Test-Path ".serverless") { Remove-Item -Recurse -Force .serverless }
 serverless deploy --stage prod
 
@@ -24,4 +24,4 @@ cd $FRONTEND_PATH
 aws s3 sync dist/ s3://stockhome-front-storage-383842796189 --region eu-west-3
 cd ..
 
-Write-Host "? Déploiement terminé avec succ?s !" -ForegroundColor Green
+Write-Host "? Dï¿½ploiement terminï¿½ avec succ?s !" -ForegroundColor Green
